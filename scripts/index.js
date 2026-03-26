d6d680229ee2d3b018f535630f0d1b0ba5f813b5
Gustavo Augusto <gustavoags2000@gmail.com>
2026-03-26 15:46:13 -0300

    feat: create functions

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,40 @@ let initialCards = [
 initialCards.forEach(function (card) {
   console.log(card);
 });
+
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeButton = editPopup.querySelector(".popup__close");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+editButton.addEventListener("click", function () {
+  handleOpenEditModal(editPopup);
+  editButton();
+});
+closeButton.addEventListener("click", function () {
+  closeModal(editPopup);
+});
+
+function fillProfileForm() {
+  let titleElement = document.querySelector(".profile__title");
+  let descriptionElement = document.querySelector(".profile__description");
+  let profileTitle = document.querySelector(".popup__input_type_name");
+  let profileDescription = document.querySelector(
+    ".popup__input_type_description",
+  );
+
+  profileTitle.value = titleElement.textContent;
+  profileDescription.value = descriptionElement.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal();
+}
