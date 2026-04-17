b7e869fb87ca1d7da470c2270c51ee552711e9cd
Victor Hugo <victor_hgsilva@hotmail.com>
2026-04-17 15:05:39 -0300

    feat: create new functions

--- scripts/index.js
+++ scripts/index.js
@@ -156,3 +156,76 @@ closeCardButton.addEventListener("click", function () {
   closeModal(addLocalPopup);
 });
 
+const typeNameInput = document.querySelector(".popup__input_type_name");
+const descriptionNameInput = document.querySelector(".popup__input_type_description");
+const button = document.querySelector(".popup__button");
+
+typeNameInput.addEventListener("input", (e) => {
+  const inputError = document.querySelector(".name-input-error");
+  if (e.target.value.length < 2 || e.target.value.length > 40) {
+    inputError.innerText = 'O campo "Nome" deve ter entre 2 e 40 caracteres'
+    button.disabled = true;
+  } else {
+    inputError.innerText = "";
+    button.disabled = false;
+  }
+});
+
+descriptionNameInput.addEventListener("input", (e) => {
+  const inputError = document.querySelector(".description-input-error");
+  if (e.target.value.length < 2 || e.target.value.length > 200) {
+    inputError.innerText = 'O campo "Sobre" deve ter entre 2 e 200 caracteres.'
+    button.disabled = true;
+  } else {
+    inputError.innerText = "";
+    button.disabled = false;
+  }
+});
+
+const titleNameInput = document.querySelector(".popup__input_type_card-name");
+const linkInput = document.querySelector(".popup__input_type_url");
+const buttonCard = addLocalPopup.querySelector(".popup__button");
+
+cardNameInput.addEventListener("input", (e) => {
+  const inputError = document.querySelector(".title-input-error");
+  if (e.target.value.length < 2 || e.target.value.length > 30) {
+    inputError.innerText = 'O campo "Título" deve ter entre 2 e 30 caracteres'
+    buttonCard.disabled = true;
+  } else {
+    inputError.innerText = "";
+    buttonCard.disabled = false;
+  }
+});
+
+linkInput.addEventListener("input", (e) => {
+  const inputError = document.querySelector(".link-input-error");
+  const inputURL = document.querySelector(".popup__input_type_url");
+  if (inputURL.checkValidity()) {
+    inputError.innerText = "";
+    buttonCard.disabled = false;
+  } else {
+    inputError.innerText = 'Insira uma URL'
+    buttonCard.disabled = true;
+  }
+});
+
+editButton.addEventListener("click", (e) => {
+  openModal(editPopup);
+});
+
+const popups = document.querySelectorAll(".popup");
+
+function handlePopClose(evt) {
+  if (evt.target.classList.contains("popup")) {
+    console.log(evt.target)
+    closeModal(evt.currentTarget);
+  }
+}
+
+popups.forEach((element) => {
+  element.addEventListener("click", handlePopClose)
+});
+
+
+
+
