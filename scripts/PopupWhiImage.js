f926077513710b2480f247101dff03853f216d4e
Gustavo Augusto <gustavoags2000@gmail.com>
2026-05-19 13:33:40 -0300

    feat: create class PopupWithImage

---
+++ scripts/PopupWhiImage.js
@@ -0,0 +1,17 @@
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
+
+  open(name, link) {
+    const image = this._popup.querySelector(".popup__image");
+    const caption = this._popup.querySelector(".popup__caption");
+
+    image.src = link;
+    image.alt = name;
+
+    caption.textContent = name;
+
+    super.open();
+  }
+}
