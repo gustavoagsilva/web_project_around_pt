e1bf5df1eb23c1b72f7be963547705a91d3634a8
Gustavo Augusto <gustavoags2000@gmail.com>
2026-06-08 12:41:42 -0300

    feat: create the api.getUserInfo in index.js

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,21 +1,23 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithConfirmation extends Popup {
-    constructor(popupSelector, handleConfirm) {
-        super(popupSelector);
-        this._handleConfirm = handleConfirm;
-        this._handleDeleteClick = this._popup.querySelector(".popup__button_delete");
-    }
+  constructor(popupSelector, handleConfirm) {
+    super(popupSelector);
+    this._handleConfirm = handleConfirm;
+    this._handleDeleteClick = this._popup.querySelector(
+      ".popup__button_delete",
+    );
+  }
 
-    open(card) {
-        this._card = card;
-        super.open();
-    }
+  open(card) {
+    this._card = card;
+    super.open();
+  }
 
-    setEventListeners() {
-        super.setEventListeners();
-        this._handleDeleteClick.addEventListener("click", () => {
-            this._handleConfirm(this._card);
-        });
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this._handleDeleteClick.addEventListener("click", () => {
+      this._handleConfirm(this._card);
+    });
+  }
 }
